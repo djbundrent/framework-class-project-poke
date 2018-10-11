56b5de2a04a9b078b3e12622780e5cdb0c94e9dc
Chris Burgin <chrisburgin@Hannahs-MacBook-Pro.local>
2018-10-11 18:09:31 -0400

    Remove

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
 import React, { Component } from "react";
-import poke from "./poke.js"
-
-console.log(poke.results)
 
 class App extends Component {
-  state = {
-    searchString: ""
-  }
-
-  onInput = (event) => {
-    this.setState({searchString: event.target.value})
-  }
-
-  generateResults = (searchString) => {
-    if (searchString === '') {
-      return []
-    } else {
-      return poke.results.filter(p => p.name.includes(searchString)).slice(0, 10)
-    }
-  }
-
   render() {
-    const results = this.generateResults(this.state.searchString)
-
     return (
       <div className="App">
-        <form>
-          <input onInput={this.onInput}></input>
-          <ul className="poke">
-            {results.map(r => <li> {r.name} </li>)}
-          </ul>
-        </form>
       </div>
     );
   }

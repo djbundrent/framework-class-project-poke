4b24f913df6f123f0b840a11ecd6e2165f9ae6cb
Devin Bundrent <devinbundrent@icloud.com>
2018-10-11 19:14:13 -0400

    added search and select features

--- src/App.js
+++ src/App.js
@@ -5,25 +5,51 @@ class App extends Component {
     super()
     this.state = {
       pokemon: [],
-      search: ""
+      search: "",
+      selectedPokemon: null
     }
   }
 
   async componentDidMount(){
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon/")
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/`)
     const json = await res.json()
     this.setState({pokemon: json.results})
   }
 
-onSearchChange = event => this.setState({search: event.target.value})
+onSearchChange = event => {
+  this.setState({search: event.target.value})
+}
+
+generateSearchResults = search => {
+  if (search === ""){
+    return []
+  } else {
+    return this.state.pokemon
+      .filter(p => p.name.includes(search))
+      .slice(0,10)
+  }
+}
+
+selectPokemon = async name => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`)
+  const json = await res.json()
+  this.setState({selectedPokemon: json})
+
+}
 
   render() {
+    const results = this.generateSearchResults(this.state.search)
+
     return (
       <div className="App">
         <div className="search">
           <input onChange={this.onSearchChange} type="text" />
           <ul>
-            your list here
+            {results.map(r => 
+              <li onClick={() => this.selectPokemon(r.name)}> 
+                { r.name } 
+              </li>
+            )}
           </ul>
         </div>
       </div>

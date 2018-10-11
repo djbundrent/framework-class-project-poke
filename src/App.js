f3337f6d7b8018da76b2bf0a16213539e5dc61f9
Devin Bundrent <devinbundrent@icloud.com>
2018-10-11 18:34:17 -0400

    set up fetch api request to gather list of pokemon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      pokemon: [],
+      search: ""
+    }
+  }
+
+  async componentDidMount(){
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon/")
+    const json = await res.json()
+    this.setState({pokemon: json.results})
+  }
+
+onSearchChange = event => this.setState({search: event.target.value})
+
   render() {
     return (
       <div className="App">
+        <div className="search">
+          <input onChange={this.onSearchChange} type="text" />
+          <ul>
+            your list here
+          </ul>
+        </div>
       </div>
     );
   }
